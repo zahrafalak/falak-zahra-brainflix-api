257f288ca9a1ebd9d81244e14533d791d0d6da7d
Falak Zahra <falak.zahra16@gmail.com>
2024-03-10 18:11:05 -0600

    test

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const videosRoute = require("./routes/videos");
+app.use("/videos", videosRoute);
+
+app.listen(port, () => console.log(`Listening on ${port}`));
